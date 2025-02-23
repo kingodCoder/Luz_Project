0be92af87afc75b63bb700776306aa3c3dd9f935
Victor <emmanuelmb261@gmail.com>
2025-02-23 22:26:40 +0200

    Mise à jour de l'AOS, ajout des sections témoignages, actualités, processus de création...

---
+++ js/script.js
@@ -0,0 +1,48 @@
+(function() {
+  "use strict";
+
+/**
+  * Testimonials slider
+  */
+  new Swiper('.testimonials-slider',
+    {
+      speed: 600,
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 40
+        },
+
+        1200: {
+          slidesPerView: 3,
+        }
+      }
+    });
+
+  /**
+  * Animation on scroll
+  */
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load',
+    () => {
+      aos_init();
+    });
+})();
